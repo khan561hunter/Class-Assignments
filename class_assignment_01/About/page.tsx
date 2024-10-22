dcfc2735979fb0b65f719815096ddd665f1265ba
Wania Khan <160246913+khan561hunter@users.noreply.github.com>
2024-10-23 02:29:45 +0500

    Delete class_assignment_01 directory

--- class_assignment_01/About/page.tsx
+++
@@ -1,9 +0,0 @@
-
-export default function About(){
-    return(
-        <div className="bg-purple-700 h-100 text-white font-semibold text-lg p-6">
-            This is About page</div>
-    )
-}
-
-
--- class_assignment_01/page.tsx
+++
@@ -1,8 +0,0 @@
-
-
-export default function Home() {
-  return (
-    <div className="bg-sky-700 h-100 text-white font-semibold text-lg p-6">
-      This is a Home page </div>
-  );
-}
